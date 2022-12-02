9689aacb95515d72f819f359a649b3a69ae10395
AndroidDev-Rehan <rehanpsycho29@gmail.com>
2022-12-02 19:24:11 +0500

    Initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+ static const Color primary =  Color.fromRGBO(49, 55, 62, 1);
+ static const Color primaryLight1 =  Color.fromRGBO(76, 86, 95, 1);
+ static const Color primaryLight2 =  Color.fromRGBO(104, 114, 125, 1);
+ static const Color primaryLight3 =  Color.fromRGBO(121, 130, 147, 1);
+ static const Color primaryLight4 =  Color.fromRGBO(147, 154, 168, 1);
+ 
+}
---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyles{
+  static const TextStyle greyTextStyle18 = TextStyle(color: Colors.grey, fontSize: 18);
+  static TextStyle greyTextStyle18Opacity50 = TextStyle(color: Colors.grey.withOpacity(0.5), fontSize: 18);
+}
